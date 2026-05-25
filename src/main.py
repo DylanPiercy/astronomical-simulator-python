66a904ed4fb35d669d306ea9f00bd487cb7dc053
DylanPiercy <dylanpiercy.3@gmail.com>
2026-05-25 11:56:09 +0100

    Feat: Added a basic scene setup with static spheres for the sun and earth.

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from simulation.scene_setup import setup_scene
+
+
+def main():
+    setup_scene()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/simulation/scene_setup.py
@@ -0,0 +1,49 @@
+"""
+Scene setup for the astronomical simulator.
+"""
+
+from vpython import color, scene, vector
+
+from config.constants import DISTANCE_SCALE
+from config.solar_system_data import (
+    EARTH,
+    EARTH_AVERAGE_ORBITAL_DISTANCE,
+    EARTH_MASS,
+    EARTH_RADIUS,
+    SUN,
+    SUN_MASS,
+    SUN_RADIUS,
+)
+from models.celestial_body import CelestialBody
+
+
+def setup_scene() -> list[CelestialBody]:
+    """
+    Sets up the initial VPython scene and creates the starting celestial bodies.
+    """
+    scene.title = "Astronomical Simulator"
+    scene.width = 1200
+    scene.height = 800
+    scene.background = color.black
+
+    sun = CelestialBody(
+        name=SUN,
+        mass=SUN_MASS,
+        radius=SUN_RADIUS,
+        position=vector(0, 0, 0),
+        velocity=vector(0, 0, 0),
+        colour=color.yellow,
+        make_trail=False,
+    )
+
+    earth = CelestialBody(
+        name=EARTH,
+        mass=EARTH_MASS,
+        radius=EARTH_RADIUS,
+        position=vector(EARTH_AVERAGE_ORBITAL_DISTANCE * DISTANCE_SCALE, 0, 0),
+        velocity=vector(0, 29_780, 0),
+        colour=color.blue,
+        make_trail=True,
+    )
+
+    return [sun, earth]
