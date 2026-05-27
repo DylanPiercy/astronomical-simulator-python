ffc11b9c4f0c75afa5248fee98116aee6f94eb5c
DylanPiercy <dylanpiercy.3@gmail.com>
2026-05-27 17:19:22 +0100

    Feat: Main entry now always checks if the system vectors have been generated and generates them if not.

--- src/main.py
+++ src/main.py
@@ -5,9 +5,15 @@ Entry point for the astronomical simulator.
 from simulation.scene_setup import setup_scene
 from simulation.simulation import Simulation
 from ui.simulation_controls import SimulationControls
+from config.local_system_state_vectors import STATE_VECTORS_GENERATED
+from tools.fetch_jpl_local_system_vectors import run_fetch_tool
 
 
 def main() -> None:
+    if not STATE_VECTORS_GENERATED:
+        print("State vectors not generated. Fetching from JPL Horizons...")
+        run_fetch_tool()
+
     bodies = setup_scene()
     simulation = Simulation(bodies)
 
